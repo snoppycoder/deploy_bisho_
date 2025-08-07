3d6f9fd80cd57fee71d6e2e1cfaec796b6593ec7
Dagmawi <dagmmike1224@gmail.com>
2025-08-07 10:59:12 +0300

    env add

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,3 @@
-let userConfig = undefined;
-try {
-  userConfig = await import("./v0-user-next.config");
-} catch (e) {
-  // ignore error
-}
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
@@ -21,38 +14,6 @@ const nextConfig = {
     parallelServerBuildTraces: true,
     parallelServerCompiles: true,
   },
-  async rewrites() {
-    const isProd = process.env.NODE_ENV === 'production';
-    const backendUrl = isProd ? process.env.BACKEND_URL : 'http://localhost:3000';
-    return [
-      {
-        source: '/api/:path*', // Match all /api/* routes
-        destination: `${backendUrl}/api/:path*`, // Proxy to Node.js backend
-      },
-    ];
-  },
 };
-
-mergeConfig(nextConfig, userConfig);
-
-function mergeConfig(nextConfig, userConfig) {
-  if (!userConfig) {
-    return;
-  }
-
-  for (const key in userConfig) {
-    if (
-      typeof nextConfig[key] === "object" &&
-      !Array.isArray(nextConfig[key])
-    ) {
-      nextConfig[key] = {
-        ...nextConfig[key],
-        ...userConfig[key],
-      };
-    } else {
-      nextConfig[key] = userConfig[key];
-    }
-  }
-}
 
 export default nextConfig;
