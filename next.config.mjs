5e60757508df856ee55a0a10bcb05ef851175d56
Dagmawi <dagmmike1224@gmail.com>
2025-08-07 11:13:46 +0300

    env add

--- next.config.mjs
+++ next.config.mjs
@@ -14,6 +14,12 @@ const nextConfig = {
     parallelServerBuildTraces: true,
     parallelServerCompiles: true,
   },
+  rewrites: async () => [
+    {
+      source: '/api/:path*',
+      destination: `${process.env.API_URL || 'http://localhost:3000'}/api/:path*`,
+    },
+  ],
 };
 
 export default nextConfig;
